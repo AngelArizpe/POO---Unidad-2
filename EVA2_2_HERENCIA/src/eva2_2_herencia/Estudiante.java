282b8479bd2804b27f9bf2e34987008c2fe19a22
AngelArizpe <124701792+AngelArizpe@users.noreply.github.com>
2023-03-11 15:24:08 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Estudiante.java
@@ -0,0 +1,42 @@
+package eva2_2_herencia;
+
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String noControl;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(String noControl) {
+        this.noControl = noControl;
+    }
+    
+    
+}
---
+++ EVA2_2_HERENCIA/src/eva2_2_herencia/Proveedores.java
@@ -0,0 +1,51 @@
+package eva2_2_herencia;
+
+public class Proveedores {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String razonSocial;
+    private String RFC;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+    
+    
+}
