ce4bdcc398767d056667b8b8c591bd9fe918f988
AngelArizpe <124701792+AngelArizpe@users.noreply.github.com>
2023-03-25 15:32:33 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,20 @@
+package eva2_12_clases_abstractas;
+
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        
+        //No se pueden crear objetos (instancias) de clases abstractas
+        //PruebaAbstracta pAbs = new PruebaAbstracta();
+    }
+    
+}
+
+class Prueba{
+    
+}
+
+abstract class PruebaAbstracta{
+    
+}
